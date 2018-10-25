b87a7a4c84686142360f9fc773f3507cffd645a7
Nguyễn Hoàng Trung Hậu <hau.nht.15dh110196@outlook.com>
2018-10-25 11:57:18 +0700

    Thêm chức năng
    Thêm vaildation và chỉnh sửa layout

--- QLMP/QLMP/Models/MyPham.cs
+++ QLMP/QLMP/Models/MyPham.cs
@@ -10,12 +10,18 @@ namespace QLMP.Models
     public class MyPham
     {
         [Key]
+        [Display(Name = "Mã mỹ phẩm")]
         public string MaMP { get; set; }
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        [Display(Name = "Tên mỹ phẩm")]
+        [StringLength(50, ErrorMessage = "Không quá 50 ký tự")]
         public string TenMP { get; set; }
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        [MaxLength(50, ErrorMessage = "Không quá 50 ký tự")]
+        [Display(Name = "Thương hiệu")]
         public string ThuongHieu { get; set; }
         [ForeignKey("CongDung")]
+        [Display(Name = "Tên công dụng")]
         public string TenCongDung { get; set; }
         public virtual CongDung CongDung { get; set; }
     }
